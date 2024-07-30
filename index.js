533a06210b8d581a6937b51c569318f1e3e805d6
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-07-30 13:38:25 -0300

    use express to route

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import http from "http";
-import url from "url";
+import express from "express";
 import fs from "fs/promises";
 import path from "path";
+
+const app = express();
+const PORT = 8081;
 
 // function to read files
 const readFileAsync = async (filePath) => {
@@ -13,43 +15,42 @@ const readFileAsync = async (filePath) => {
   }
 };
 
-//  404 page
+// 404 page
 let page404;
 readFileAsync("./pages/404.html").then((data) => {
   page404 = data;
 });
 
+
 // function to serve a file
 const serveFile = async (res, filePath, contentType = "text/html") => {
   try {
     const data = await readFileAsync(filePath);
-    res.writeHead(200, { "Content-Type": contentType });
-    res.end(data);
+    res.status(200).contentType(contentType).send(data);
   } catch (err) {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end(page404);
+    res.status(404).contentType("text/html").send(page404);
   }
 };
 
-//server
-const server = http.createServer(async (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-
-  let filename;
-  if (parsedUrl.pathname === "/") {
-    filename = "./pages/index.html";
-  } else if (req.url === "/about") {
-    filename = "./pages/about.html";
-  } else if (req.url === "/contact") {
-    filename = "./pages/contact-me.html";
-  } else {
-    filename = path.join("./pages", parsedUrl.pathname);
-  }
-
-  console.log(filename);
-  await serveFile(res, filename);
+// routes
+app.get("/", async (req, res) => {
+  await serveFile(res, "./pages/index.html");
 });
 
-server.listen(8080, () => {
-  console.log("Server is working!");
+app.get("/about", async (req, res) => {
+  await serveFile(res, "./pages/about.html");
+});
+
+app.get("/contact", async (req, res) => {
+  await serveFile(res, "./pages/contact-me.html");
+});
+
+app.get("*", async (req, res) => {
+  const filePath = path.join("./pages", req.path);
+  await serveFile(res, filePath);
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server is working on port ${PORT}!`);
 });
